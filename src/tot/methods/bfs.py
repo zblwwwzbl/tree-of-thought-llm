ea82d589d32cd3afa733a2e9605ce36ffa596f77
notalol1con <zbl183905063@gmail.com>
2024-12-11 12:44:34 -0500

    x

--- src/tot/methods/bfs.py
+++ src/tot/methods/bfs.py
@@ -1,7 +1,7 @@
 import itertools
 import numpy as np
 from functools import partial
-from tot.huggingface_model import huggingface_gpt
+from huggingface_model import huggingface_gpt
 
 def get_value(task, x, y, n_evaluate_sample, cache_value=True):
     value_prompt = task.value_prompt_wrap(x, y)
